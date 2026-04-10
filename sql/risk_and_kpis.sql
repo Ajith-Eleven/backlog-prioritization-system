8c46c46f99f25c04b1a4d6122be1fb4aa46b6e90
Ajith Sakamuri <ajithsakamuri1103@gmail.com>
2026-04-10 15:43:26 +0530

    Add risk identification and KPI computation query
    Added a query to identify risks and compute KPIs for backlog items.

--- sql/risk_and_kpis.sql
+++ sql/risk_and_kpis.sql
@@ -1,1 +1,14 @@
 
+-- Identify risk + compute KPIs
+
+SELECT
+  id,
+  DATE_DIFF(CURRENT_DATE(), DATE(created_at), DAY) AS days_in_backlog,
+
+  CASE
+    WHEN implemented_at IS NULL
+    AND DATE_DIFF(CURRENT_DATE(), DATE(created_at), DAY) > 30
+    THEN 1 ELSE 0
+  END AS at_risk
+
+FROM backlog_items;
